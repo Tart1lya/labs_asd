edb621b10129291eecaf46bc7fc71b7e508256e7
Tart1lya <iiigametv2006@gmail.com>
2024-09-14 22:02:28 +0300

    fix fib alg

--- speedtest/fib2test.py
+++ speedtest/fib2test.py
@@ -1,13 +1,15 @@
+import tracemalloc
 import time
 t_start = time.perf_counter()
+tracemalloc.start()
 f = open('inputfibtest.txt')
 n = int(f.readline())
 f1 = open('outputfibtest.txt', 'w')
 if 0 <= n <= 10**7:
     a, b = 0, 1
     for i in range(2, n + 1):
-        a, b = b, a + b
-    f1.write(str(b % 10))
-f1.close()
-
-print("Время работы: %s секунд " % (time.perf_counter() - t_start))
+        a, b = b % 10, (a + b) % 10
+    f1.write(str(b))
+print("Время работы: %s секунд" % (time.perf_counter() - t_start))
+print("Затрачено памяти:", tracemalloc.get_traced_memory()[1], "байт")
+tracemalloc.stop()
